448fc4baf6a53c061a171ac051b3979f73ea6d00
Your Name <you@example.com>
2022-09-28 17:29:15 +0800

    加写备注

--- backend/index.php
+++ backend/index.php
@@ -18,14 +18,33 @@
 	    'charset' => 'utf8',
 	    'port' => $dbconfig['port'],
 	]);
-	// 构建路由
+	// 构建路由 默认路由index
 	if (!isset($_GET['route'])) {
 		$route = 'index';
+	} else {
+		$route = $_GET['route'];
 	}
+
+	// 统一返回方法
+	function ajaxResponse($code = '200', $data = [], $msg = '') {
+		echo json_encode( [
+			'code' => $code,
+			'data' => $data,
+			'msg' => $msg
+		]);
+	}
+
+	// 根据ip获取用户
+	// 找不到用户提示联系系统管理员
+
+	// 首页
 	if ($route == 'index') {
-		// 首页
+		// 查看用户今天投票的情况
+		// 查看今天的投票选项
+		// 选项没有生成提示请4点以后再来
 	} else if ($route == 'toupiao') {
 		// 投票
+		// 发送投票通知 (xxxxxx已经投票)
 	} else {
 		echo 404;
 	}
