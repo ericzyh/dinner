8fb73d7063dfe0e764c7405e3c26b05645a58c23
赵宇豪 <8040334@qq.com>
2022-09-28 16:30:54 +0800

    Create db.config.php

---
+++ backend/db.config.php
@@ -0,0 +1,8 @@
+<?php
+        return [
+                'host' => 'localhost',
+                'port' => '3306',
+                'name' => 'test',
+                'username' => 'root',
+                'password' => '****'
+        ];
